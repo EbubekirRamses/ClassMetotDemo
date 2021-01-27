f33a248483a381ce60e63b503e185f880065e880
Ahmet Metin Bahçeci <ahmetmetinbahceci@gmail.com>
2021-01-27 22:03:37 +0300

    projem

---
+++ MusteriServis.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriServis
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Yeni Müşteri :" + musteri.Ad);
+        }
+
+        
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Şevval";
+            musteri1.Soyad = "Özdemir";
+            musteri1.Id = 125672;
+
+            //11 sene oldu gel artık :(
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Metin";
+            musteri2.Soyad = "Bahçeci";
+            musteri2.Id = 16860254;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Kadir";
+            musteri3.Soyad = "AyranÇeynemez";
+            musteri3.Id = 513154354;
+
+            Musteri[] musteriler = new Musteri[] { musteri1,musteri2,musteri3};
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Kullanıcı Adınızı Giriniz :" + musteri1.Ad);
+                Console.WriteLine("Kullanıcı Soyadını Giriniz :" + musteri1.Soyad);
+                Console.WriteLine("Lütfen Parolayı Giriniz!" + musteri1.Id);
+                Console.WriteLine("----------------------------------");
+
+                Console.WriteLine("Kullanıcı Adınızı Giriniz :" + musteri2.Ad);
+                Console.WriteLine("Kullanıcı Soyadını Giriniz :" + musteri2.Soyad);
+                Console.WriteLine("Lütfen Parolayı Giriniz!" + musteri2.Id);
+                Console.WriteLine("----------------------------------");
+
+                Console.WriteLine("Kullanıcı Adınızı Giriniz :" + musteri3.Ad);
+                Console.WriteLine("Kullanıcı Soyadını Giriniz :" + musteri3.Soyad);
+                Console.WriteLine("Lütfen Parolayı Giriniz!" + musteri3.Id);
+                Console.WriteLine("----------------------------------");
+
+            }
+            
+
+        }
+    }
+}
